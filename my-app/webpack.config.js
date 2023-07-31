7af32b0abfd1ff24daef5dfb46d3d0491b41464d
Michal Malinowski <michumalinowski@gmail.com>
2023-07-31 22:55:18 +0200

    probably solved problem with wite screen after deployment

--- my-app/webpack.config.js
+++ my-app/webpack.config.js
@@ -1,36 +1,51 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'output'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        "presets": [
-                            "@babel/preset-env"
-                          ]
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            filename: "./index.html"
-         })
+module.exports = ({ mode } = { mode: "production" }) => {
+    console.log(`mode is: ${mode}`);
+    module:{
+        rules:[{
+            loader: 'babel-loader',
+            test: /\.js$|jsx/,
+            exclude: /node_modules/
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+            
+    
     ]
+    }
+    
+    return {
+            mode,
+            entry: "./src/index.js",
+            output: {
+                publicPath: "/",
+                path: path.resolve(__dirname, "build"),
+                filename: "bundled.js"
+            },
+            resolve: {
+                extensions: ['', '.js', '.jsx']
+              },
+              module: {
+                rules: [
+                  {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                  },
+                  {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                  },
+                ]
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    template: "./public/index.html"
+                }),
+            ]
+        }
 }
